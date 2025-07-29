d5eb1810221a42cc655efbd0ba7a8065829d959b
AspaNeuro <a.almeida@neurogears.org>
2025-07-29 11:34:02 +0100

    Add bonsai node to get wintab events into editor

---
+++ src/Bonsai.Wintab/WinTabCapture.cs
@@ -0,0 +1,45 @@
+﻿using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Reactive.Subjects;
+using WintabDN;
+
+namespace Bonsai.Wintab
+{
+    /// <summary>
+    /// Represents an operator that produces a sequence of values.
+    /// </summary>
+    [Description("Produces a sequence of values from a Wacom Device.")]
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class WinTabCapture
+    {
+        /// <summary>
+        /// "Produces a sequence of values from a Wacom Device."
+        /// </summary>
+        /// <returns>
+        /// WacomWintabDN data
+        /// </returns>
+        //private WintabDN.CWintabContext logContext = null;
+        //private CWintabData wtData = null;
+
+
+        public bool MouseControl { get; set; }
+        public IObservable<WintabPacket> Generate()
+        {
+            var wacom = new InterceptWinTab(MouseControl);
+
+            return wacom.WacomData;
+
+        }
+       
+
+        
+    }
+}
